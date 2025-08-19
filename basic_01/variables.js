fa03f68caac532ee740cb16ad0520d7fa688683d
Shubham <shub88510@gmail.com>
2025-08-19 23:38:30 +0530

    basic_01

---
+++ basic_01/variables.js
@@ -0,0 +1,19 @@
+const accoundId = 1445556
+// accountId = 2 //not allowed
+console.log(accoundId)
+
+let accountEmail = "shubham@gmail.com"
+accountEmail = "hc@hc.mail"
+// console.log(accountEmail)
+
+var accountPassword = "123456"
+accountPassword = "212121"
+
+accountCity = "Jaipur"
+accountCity = "Patna"
+
+console.table([accoundId,accountEmail,accountPassword,accountCity])
+
+
+
+
