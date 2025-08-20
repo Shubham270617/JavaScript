9f1ca641f3d7d0e6229b210dd29abecd6734f3cd
Shubham <shub88510@gmail.com>
2025-08-20 21:32:05 +0530

    Completed_variables

--- basic_01/variables.js
+++ basic_01/variables.js
@@ -15,5 +15,14 @@ accountCity = "Patna"
 console.table([accoundId,accountEmail,accountPassword,accountCity])
 
 
-
+// output
+// ┌─────────┬──────────────┐
+// │ (index) │ Values       │
+// ├─────────┼──────────────┤
+// │ 0       │ 1445556      │
+// │ 1       │ 'hc@hc.mail' │
+// │ 2       │ '212121'     │
+// │ 3       │ 'Patna'      │
+// We can say that the data of every variable has been printed but even when var let are declare different value is being assigned and in account city.
+// Var are not used mainly because of issue in block scope and functional scope.
 
