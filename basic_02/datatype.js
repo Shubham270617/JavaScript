f414052bfdc659710aeda38cb66422dfc2c0ac88
Shubham <shub88510@gmail.com>
2025-08-20 22:54:13 +0530

    completed_datatype

---
+++ basic_02/datatype.js
@@ -0,0 +1,42 @@
+// // Datatype and ECMA Script
+
+
+// When you write "use strict" this is treated all as newer version of javascript you can write or not it is a good practice to write. Code readability is important 
+
+// ECMA script is the organisation where standers are written not javascript. It s hard mainly for who writes the engines.
+
+// Datatypes: In JavaScript, data types categorize the different kinds of values that can be stored and manipulated. Understanding data types is fundamental for writing correct and efficient code. JavaScript data types are broadly categorized into two groups: Primitive and Non-Primitive (or Reference) data types. 
+
+// Primitive Datatypes: numbers (124), String (eg: "hello"), Boolean(true or false), Undefined(declared but not assigned), Null(absence of any object value), Symbols, BigInt(12364789), 
+
+// Non-primitive: complex data structure and mutable like array, objects function
+
+
+"use strict"  // strict mode 
+
+console.log("Shubham")
+
+// datatype
+
+let name = "Shubham"
+let age = 18
+let isLoggedIn = false
+
+
+//Primitive Datatype
+//number => 2 to power 532
+// bigint
+// string
+// undefined => value is not assigned
+// null - stand alone value 
+// boolean
+// symbols- use for uniquness 
+
+//Non primitive data type
+//Array
+//Function
+//objects
+//typeof 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
