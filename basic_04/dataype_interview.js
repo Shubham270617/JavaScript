38f5fdce1a1e8893f1d7f682ddbf509359b2fbaf
Shubham <shub88510@gmail.com>
2025-09-25 05:54:35 +0530

    DataType_Done

---
+++ basic_04/dataype_interview.js
@@ -0,0 +1,19 @@
+// Primitive Datatype|
+// 7 types: string, number, boolean, null, undefine, BigInt, Symbols
+
+//refernce type (Nono-primitive datatype)
+//  Array, Object and Functions
+
+// Javascript is dynamically type language 
+
+const heros = ["shaktiman", "hanumajee"]
+let myObj = {
+    name: "shubham",
+    age: 25
+}
+
+const myFun = function (){
+    console.log("Hello World") // datatype will be function
+}
+
+// if we write the typeof beside any function it will give me the waht type of value it is 
