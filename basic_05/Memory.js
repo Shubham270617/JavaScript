411100b19d68d313a56c536bf3ad045a07e3236e
Shubham <shub88510@gmail.com>
2025-10-03 20:57:10 +0530

    Done_Memory

---
+++ basic_05/Memory.js
@@ -0,0 +1,19 @@
+// Stack (Primitive) and Heap (Non Prmitive )
+
+// stack says that whenever the memory is declared we get a copy of it
+// Heap saya it will give refernce
+
+let myName = "shbhamsinha" 
+let anothername = myName
+
+anothername = "spidy"
+
+console.log(myName); //shubhamsinha
+console.log(anothername); //spidy
+
+
+let userOne = {
+    email: "user@gmil.com",
+    upi: "@ybl"
+
+}
