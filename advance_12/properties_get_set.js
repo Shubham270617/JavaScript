0d56ae0a403a3f046aa062550dabe45081849544
Shubham <shub88510@gmail.com>
2025-12-03 21:42:28 +0530

    completed_JS

---
+++ advance_12/properties_get_set.js
@@ -0,0 +1,26 @@
+function User(email, password){
+    this._email = email; // _ defines that we are defining it in personal property
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
